999cf9b92e8e4986dd8adf5074c8e81192ae7d92
LacyS3eeker <maraandreea2313@gmail.com>
2024-12-23 17:07:37 +0200

    .

--- pagini-materii/redirect.js
+++ pagini-materii/redirect.js
@@ -42,48 +42,47 @@ document.getElementById("clasai").onclick = function() {
   };
 
 
-
-  document.getElementById("rombac").onclick = function() {
-    window.location.href = "romana/rombac.html";
+document.getElementById("rombac").onclick = function() {
+    window.location.href = "pagini-materii/romana/rombac.html";
   };
   document.getElementById("romclasai").onclick = function() {
-    window.location.href = "romana/romclasai.html";
+    window.location.href = "pagini-materii/romana/romclasai.html";
   };
   document.getElementById("romclasaii").onclick = function() {
-    window.location.href = "romana/romclasaii.html";
+    window.location.href = "pagini-materii/romana/romclasaii.html";
   };
   document.getElementById("romclasaiii").onclick = function() {
-    window.location.href = "romana/romclasaiii.html";
+    window.location.href = "pagini-materii/romana/romclasaiii.html";
   };
   document.getElementById("romclasaiv").onclick = function() {
-    window.location.href = "romana/romclasaiv.html";
+    window.location.href = "pagini-materii/romana/romclasaiv.html";
   };
   document.getElementById("romclasav").onclick = function() {
-    window.location.href = "romana/romclasav.html";
+    window.location.href = "pagini-materii/romana/romclasav.html";
   };
   document.getElementById("romclasavi").onclick = function() {
-    window.location.href = "romana/romclasavi.html";
+    window.location.href = "pagini-materii/romana/romclasavi.html";
   };
   document.getElementById("romclasavii").onclick = function() {
-    window.location.href = "romana/romclasavii.html";
+    window.location.href = "pagini-materii/romana/romclasavii.html";
   };
   document.getElementById("romclasaviii").onclick = function() {
-    window.location.href = "romana/romclasaviii.html";
+    window.location.href = "pagini-materii/romana/romclasaviii.html";
   };
   document.getElementById("romclasaix").onclick = function() {
-    window.location.href = "romana/romclasaix.html";
+    window.location.href = "pagini-materii/romana/romclasaix.html";
   };
   document.getElementById("romclasaxi").onclick = function() {
-    window.location.href ="romana/romclasaxi.html";
+    window.location.href ="pagini-materii/romana/romclasaxi.html";
   };
   document.getElementById("romclasax").onclick = function() {
-    window.location.href = "romana/romclasax.html";
+    window.location.href = "pagini-materii/romana/romclasax.html";
   };
   document.getElementById("romclasaxii").onclick = function() {
-    window.location.href = "romana/romclasaxii.html";
+    window.location.href = "pagini-materii/romana/romclasaxii.html";
   };
   document.getElementById("romen").onclick = function() {
-    window.location.href = "romana/romen.html";
+    window.location.href = "pagini-materii/romana/romen.html";
   };
 
 
