2c84522b10ced146d659862b04d767cef4f93673
LacyS3eeker <maraandreea2313@gmail.com>
2024-12-23 16:40:04 +0200

    .

---
+++ pagini-materii/redirect.js
@@ -0,0 +1,89 @@
+document.getElementById("clasai").onclick = function() {
+    window.location.href = "matematica/clasai.html";
+  };
+  document.getElementById("clasaii").onclick = function() {
+    window.location.href = "matematica/clasaii.html";
+  };
+  document.getElementById("clasaiii").onclick = function() {
+    window.location.href = "matematica/clasaiii.html";
+  };
+  document.getElementById("clasaiv").onclick = function() {
+    window.location.href = "matematica/clasaiv.html";
+  };
+  document.getElementById("clasav").onclick = function() {
+    window.location.href = "matematica/clasav.html";
+  };
+  document.getElementById("clasavi").onclick = function() {
+    window.location.href = "matematica/clasavi.html";
+  };
+  document.getElementById("clasavii").onclick = function() {
+    window.location.href = "matematica/clasavii.html";
+  };
+  document.getElementById("clasaviii").onclick = function() {
+    window.location.href = "matematica/clasaviii.html";
+  };
+  document.getElementById("clasaix").onclick = function() {
+    window.location.href = "matematica/clasaix.html";
+  };
+  document.getElementById("clasax").onclick = function() {
+    window.location.href = "matematica/clasax.html";
+  };
+  document.getElementById("clasaxi").onclick = function() {
+    window.location.href ="matematica/clasaxi.html";
+  };
+  document.getElementById("clasaxii").onclick = function() {
+    window.location.href = "matematica/clasaxii.html";
+  };
+  document.getElementById("mateen").onclick = function() {
+    window.location.href = "matematica/mateen.html";
+  };
+  document.getElementById("matebac").onclick = function() {
+    window.location.href = "matematica/matebac.html";
+  };
+
+
+
+  document.getElementById("rombac").onclick = function() {
+    window.location.href = "romana/rombac.html";
+  };
+  document.getElementById("romclasai").onclick = function() {
+    window.location.href = "romana/romclasai.html";
+  };
+  document.getElementById("romclasaii").onclick = function() {
+    window.location.href = "romana/romclasaii.html";
+  };
+  document.getElementById("romclasaiii").onclick = function() {
+    window.location.href = "romana/romclasaiii.html";
+  };
+  document.getElementById("romclasaiv").onclick = function() {
+    window.location.href = "romana/romclasaiv.html";
+  };
+  document.getElementById("romclasav").onclick = function() {
+    window.location.href = "romana/romclasav.html";
+  };
+  document.getElementById("romclasavi").onclick = function() {
+    window.location.href = "romana/romclasavi.html";
+  };
+  document.getElementById("romclasavii").onclick = function() {
+    window.location.href = "romana/romclasavii.html";
+  };
+  document.getElementById("romclasaviii").onclick = function() {
+    window.location.href = "romana/romclasaviii.html";
+  };
+  document.getElementById("romclasaix").onclick = function() {
+    window.location.href = "romana/romclasaix.html";
+  };
+  document.getElementById("romclasaxi").onclick = function() {
+    window.location.href ="romana/romclasaxi.html";
+  };
+  document.getElementById("romclasax").onclick = function() {
+    window.location.href = "romana/romclasax.html";
+  };
+  document.getElementById("romclasaxii").onclick = function() {
+    window.location.href = "romana/romclasaxii.html";
+  };
+  document.getElementById("romen").onclick = function() {
+    window.location.href = "romana/romen.html";
+  };
+
+
